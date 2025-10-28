e0c4b53b382ef2d8002b6f61eedd30d6b9fe38db
Dominik <dominik-golenja@hotmail.com>
2025-10-28 16:21:51 +0100

    simple char stats, simple logic board

---
+++ src/PlayerMap.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PlayerMap {
+
+    private class Map{
+        private int rows;
+        private int cols;
+        private char[][] grid;
+
+        Map(int rows, int cols){
+            this.rows = rows;
+            this.cols = cols;
+            this.grid = new char[rows][cols];
+
+        }
+    }
+    public int GetRows(int rows){
+        return rows;
+    }
+    public int GetCols(Map map){
+        return map.cols;
+    }
+
+    public int SetRows(int rows){
+
+        return rows = GetRows(rows);
+    }
+
+
+    private void createmap(){
+
+    }
+
+
+}
